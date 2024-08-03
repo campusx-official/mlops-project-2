449b4ee5a9a2fa10353e519979c9191abffdc50c
nitishksingh24 <nitishksingh24@gmail.com>
2024-08-03 14:38:41 +0530

    x

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template,request
 import mlflow
 from flask_app.preprocessing_utility import normalize_text
-import dagshub
 import pickle
 import os
 import pandas as pd
 
+# Set up DagsHub credentials for MLflow tracking
 dagshub_token = os.getenv("DAGSHUB_PAT")
 if not dagshub_token:
     raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
@@ -22,7 +22,7 @@ mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 app = Flask(__name__)
 
-# Load model from model registry
+# load model from model registry
 def get_latest_model_version(model_name):
     client = mlflow.MlflowClient()
     latest_version = client.get_latest_versions(model_name, stages=["Production"])
@@ -36,33 +36,34 @@ model_version = get_latest_model_version(model_name)
 model_uri = f'models:/{model_name}/{model_version}'
 model = mlflow.pyfunc.load_model(model_uri)
 
-# Get the model's input schema
-input_schema = model.metadata.get_input_schema()
+vectorizer = pickle.load(open('models/vectorizer.pkl','rb'))
 
-vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
+
 
 @app.route('/')
 def home():
-    return render_template('index.html', result=None)
+    return render_template('index.html',result=None)
 
 @app.route('/predict', methods=['POST'])
 def predict():
+
     text = request.form['text']
 
-    # Clean
+    # clean
     text = normalize_text(text)
 
-    # Bag of Words
+    # bow
     features = vectorizer.transform([text])
 
-    # Convert sparse matrix to DataFrame with correct column names
-    features_df = pd.DataFrame.sparse.from_spmatrix(features, columns=input_schema.input_names())
+    # Convert sparse matrix to DataFrame
+    features_df = pd.DataFrame.sparse.from_spmatrix(features)
+    features_df = pd.DataFrame(features.toarray(), columns=[str(i) for i in range(features.shape[1])])
 
-    # Prediction
+    # prediction
     result = model.predict(features_df)
 
-    # Show
+    # show
     return render_template('index.html', result=result[0])
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
