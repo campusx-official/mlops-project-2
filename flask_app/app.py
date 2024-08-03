d9dc244fab14a0d1d596fb07dbdf99f55ded1147
nitishksingh24 <nitishksingh24@gmail.com>
2024-08-03 14:26:06 +0530

    x

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,28 +1,17 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 import mlflow
 from flask_app.preprocessing_utility import normalize_text
+import dagshub
 import pickle
 import os
 import pandas as pd
 
-# Set up DagsHub credentials for MLflow tracking
-dagshub_token = os.getenv("DAGSHUB_PAT")
-if not dagshub_token:
-    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
-
-os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
-
-dagshub_url = "https://dagshub.com"
-repo_owner = "campusx-official"
-repo_name = "mlops-project-2"
-
-# Set up MLflow tracking URI
-mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+mlflow.set_tracking_uri('https://dagshub.com/campusx-official/mlops-project-2.mlflow')
+dagshub.init(repo_owner='campusx-official', repo_name='mlops-project-2', mlflow=True)
 
 app = Flask(__name__)
 
-# load model from model registry
+# Load model from model registry
 def get_latest_model_version(model_name):
     client = mlflow.MlflowClient()
     latest_version = client.get_latest_versions(model_name, stages=["Production"])
@@ -36,33 +25,33 @@ model_version = get_latest_model_version(model_name)
 model_uri = f'models:/{model_name}/{model_version}'
 model = mlflow.pyfunc.load_model(model_uri)
 
-vectorizer = pickle.load(open('models/vectorizer.pkl','rb'))
+# Get the model's input schema
+input_schema = model.metadata.get_input_schema()
 
-
+vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html',result=None)
+    return render_template('index.html', result=None)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-
     text = request.form['text']
 
-    # clean
+    # Clean
     text = normalize_text(text)
 
-    # bow
+    # Bag of Words
     features = vectorizer.transform([text])
 
-    # Convert sparse matrix to DataFrame
-    features_df = pd.DataFrame.sparse.from_spmatrix(features)
+    # Convert sparse matrix to DataFrame with correct column names
+    features_df = pd.DataFrame.sparse.from_spmatrix(features, columns=input_schema.input_names())
 
-    # prediction
+    # Prediction
     result = model.predict(features_df)
 
-    # show
+    # Show
     return render_template('index.html', result=result[0])
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
